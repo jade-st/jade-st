18ae8e9565162a599f9c4b99b2f37e0c1528b7d7
j-sudell <17751176+j-sudell@users.noreply.github.com>
2024-02-06 13:12:41 +0000

    Updating astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://jades.work",
+  site: "https://jade-st.github.io",
   integrations: [tailwind()],
 });
