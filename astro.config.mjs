ebdfee2a667191beaf88125248e12759763fce43
j-sudell <17751176+j-sudell@users.noreply.github.com>
2024-02-06 13:30:47 +0000

    Github workflow changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://jade-st.github.io",
+  site: "https://jades.work/",
   integrations: [tailwind()],
 });
